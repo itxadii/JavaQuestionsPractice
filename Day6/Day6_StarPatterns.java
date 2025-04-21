a76460a1fb3373e72bb3be3bd3d79bc15e6b3007
itxadii <diamondgamerz49@gmail.com>
2025-04-22 01:21:26 +0530

    Done Branching

---
+++ Day6/Day6_StarPatterns.java
@@ -0,0 +1,28 @@
+package Day6;
+
+public class Day6_StarPatterns {
+    public static void main(String args[]) {
+        // for(int line=1;line<=n;line++){
+        // for(int star=1;star<=line;star++){
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // }
+
+        // for(int line=1;line<=n;line++){
+        // for(int star=1;star<=(n-line+1);star++){
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // }
+        int n = 4;
+        char ch = 'A';
+        for (int line = 1; line <= n; line++) {
+            for (int chars = 1; chars <= line; chars++) {
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
