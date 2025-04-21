efbc6314cfab7075dc92d9a53960d24f345ef5f0
itxadii <diamondgamerz49@gmail.com>
2025-04-22 01:06:44 +0530

    Day6

--- Desktop/Java Programming/JavaPractice/Day6.java
+++ Desktop/Java Programming/JavaPractice/Day6.java
@@ -1,13 +1,26 @@
 package JavaPractice;
+
 public class Day6 {
     public static void main(String args[]) {
+        // for(int line=1;line<=n;line++){
+        // for(int star=1;star<=line;star++){
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // }
+
+        // for(int line=1;line<=n;line++){
+        // for(int star=1;star<=(n-line+1);star++){
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // }
         int n = 4;
         char ch = 'A';
         for (int line = 1; line <= n; line++) {
-			for (int chars = 1; chars <= line; chars++) {
+            for (int chars = 1; chars <= line; chars++) {
                 System.out.print(ch);
-				ch++;
-				
+                ch++;
             }
             System.out.println();
         }
