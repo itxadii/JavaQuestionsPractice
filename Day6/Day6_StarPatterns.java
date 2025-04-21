e042722334930b3a8e40ec899c9c0c68cc4a8eac
itxadii <diamondgamerz49@gmail.com>
2025-04-22 01:46:31 +0530

    Star Patterns Logic

--- Day6/Day6_StarPatterns.java
+++ Day6/Day6_StarPatterns.java
@@ -4,7 +4,7 @@ public class Day6_StarPatterns {
     public static void main(String args[]) {
         // for(int line=1;line<=n;line++){
         // for(int star=1;star<=line;star++){
-        // System.out.print("*");
+		// System.out.print("*");
         // }
         // System.out.println();
         // }
