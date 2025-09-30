9951184685642892c37313b090efaebeef813394
itxadii <diamondgamerz49@gmail.com>
2025-10-01 01:21:10 +0530

    Printing Subarrays

--- Day9/PrintSubarrays.java
+++ Day9/PrintSubarrays.java
@@ -1,20 +1,23 @@
 package Day9;
 
-public class PrintSubrrays {
+public class PrintSubarrays {
     public static void main(String[] args) {
         int numbers[] = {2, 4, 6, 8, 10};
         printSubrrays(numbers);
     }
     public static void printSubrrays (int numbers[]) {
+        int ts = 0;
         for(int i=0; i < numbers.length; i++) {
             for(int j=i; j < numbers.length; j++) {
                 for(int k=i; k <= j; k++) {
                     System.out.print(numbers[k] + " ");
                 }
+            ts++;
             System.out.println();
             }
         System.out.println();
         }
+        System.out.println("Total subarrays = " + ts);
 
     }
 
