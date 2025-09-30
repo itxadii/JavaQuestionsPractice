bb6331ad34135cf1467f5b696e5792cf519f2089
itxadii <diamondgamerz49@gmail.com>
2025-10-01 00:26:23 +0530

    Print Pairs of Array

---
+++ Day9/PrintPairs.java
@@ -0,0 +1,19 @@
+package Day9;
+
+public class PrintPairs {
+    public static void main(String args[]) {
+        int numbers[] = {2, 4, 6, 8, 10};
+        printPairs(numbers);
+    }
+    public static void printPairs(int numbers[]) {
+        int tp = 0;
+        for(int i=0; i<numbers.length; i++) {
+            for(int j=i+1; j<numbers.length; j++) {
+                System.out.println("(" + numbers[i] + ", " + numbers[j] + ")");
+                tp++;
+            }
+        }
+        System.out.println();
+        System.out.println(" Total pairs = " + tp);
+    }
+}
