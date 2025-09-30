0c54ee43113220001e15fa2fa8c59bc751cc2868
itxadii <diamondgamerz49@gmail.com>
2025-10-01 00:31:16 +0530

    Pairs Printing

--- Day9/PrintPairs.java
+++ Day9/PrintPairs.java
@@ -9,11 +9,11 @@ public class PrintPairs {
         int tp = 0;
         for(int i=0; i<numbers.length; i++) {
             for(int j=i+1; j<numbers.length; j++) {
-                System.out.println("(" + numbers[i] + ", " + numbers[j] + ")");
+                System.out.print("(" + numbers[i] + ", " + numbers[j] + ")");
                 tp++;
             }
+            System.out.println();
         }
-        System.out.println();
-        System.out.println(" Total pairs = " + tp);
+        System.out.println("Total pairs = " + tp);
     }
 }
